958af31e04f3e3a3f69909fcea18bf63c570710a
Caducus <stefanoskissas@hotmail.com>
2024-10-21 14:52:59 +0300

    modules

---
+++ script/DeployCaducusToken.s.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {CaducusToken} from "../src/CaducusToken.sol";
+
+contract DeployCaducusToken is Script {
+    uint256 public constant INITIAL_SUPPLY = 1000;
+
+    function run() external returns (CaducusToken) {
+        vm.startBroadcast();
+        CaducusToken caducusToken = new CaducusToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+        return caducusToken;
+    }
+}
--- src/CaducusToken.sol
+++ src/CaducusToken.sol
@@ -1,6 +1,10 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
-import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CaducusToken {}
+contract CaducusToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("CaducusToken", "CDC") {
+        _mint(msg.sender, initialSupply);
+    }
+}
